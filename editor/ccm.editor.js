74e22c32e86eeb1ee5047377af68f410f9e93f80
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-06-03 20:52:48 +0200

    migrate to ccm v.8

--- editor/ccm.editor.js
+++ editor/ccm.editor.js
@@ -3,32 +3,47 @@
  * @author Tea Kless <tea.kless@web.de>, 2017
  * @license The MIT License (MIT)
  */
-ccm.component ( {
-  name: 'editor',
-  config: {
-    editor: [ ccm.load, '//cdn.quilljs.com/1.2.0/quill.min.js' ],
-    editor_css: [ ccm.load, '//cdn.quilljs.com/1.2.0/quill.snow.css' ],
-    settings: {
-      modules: {
-        toolbar: [
-          [ { header: [ 1, 2, false ] } ],
-          [ 'bold', 'italic', 'underline' ],
-          [ 'image', 'code-block' ]
-        ]
-      },
-      placeholder: 'Write here...',
-      theme: 'snow'
+
+( function () {
+
+  var ccm_version = '8.0.0';
+  var ccm_url     = '../libs/ccm.js';
+
+  var component_name = 'editor';
+  var component_obj  = {
+    name: component_name,
+
+    config: {
+      editor: [ 'ccm.load', '//cdn.quilljs.com/1.2.0/quill.min.js' ],
+      editor_css: [ 'ccm.load', '//cdn.quilljs.com/1.2.0/quill.snow.css' ],
+      settings: {
+        modules: {
+          toolbar: [
+            [ { header: [ 1, 2, false ] } ],
+            [ 'bold', 'italic', 'underline' ],
+            [ 'image', 'code-block' ]
+          ]
+        },
+        placeholder: 'Write here...',
+        theme: 'snow'
+      }
+    },
+
+    Instance: function () {
+      var editor;
+
+      this.start = function ( callback ) {
+        var div = this.ccm.helper.html( {} );
+        this.element.innerHTML = '';
+        this.element.appendChild( div );
+        editor = new Quill( div, this.settings );
+        if ( callback ) callback( this );
+      };
+      this.get = function () { return editor; }
     }
-  },
-  Instance: function () {
-    var editor;
-    this.render = function ( callback ) {
-      var div = ccm.helper.html( {} );
-      this.element.innerHTML = '';
-      this.element.appendChild( div );
-      editor = new Quill( div, this.settings );
-      if ( callback ) callback( this );
-    };
-    this.get = function () { return editor; }
-  }
-} );
+  };
+
+  var namespace = window.ccm && ccm.components[ component_name ]; if ( namespace ) { if ( namespace.ccm_version ) ccm_version = namespace.ccm_version; if ( namespace.ccm_url ) ccm_url = namespace.ccm_url; }
+  if ( !window.ccm || !ccm[ ccm_version ] ) { var tag = document.createElement( 'script' ); document.head.appendChild( tag ); tag.onload = register; tag.src = ccm_url; } else register();
+  function register() { ccm[ ccm_version ].component( component_obj ); }
+}() );
