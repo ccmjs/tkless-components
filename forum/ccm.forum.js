0379c493e59b4c333be1df221fa0885f9c3c4e0d
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-07-12 22:41:06 +0200

    fill in the blank-text factory

--- forum/ccm.forum.js
+++ forum/ccm.forum.js
@@ -260,7 +260,6 @@
             }
           }
 
-
           function renderEditor() {
             self.editor.start( { root: self.element.querySelector( '#editor-container' ) }, function ( instance ) {
               editor = instance;
