da39fa440734763cac5f8736c079783db2d4ebac
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-08-19 00:27:59 +0200

    set absolute paths

---
+++ question/question_configs.js
@@ -0,0 +1,15 @@
+ccm.files[ "question_configs.js" ] = {
+  "1": {
+    "data": {
+      "store": [ "ccm.store", "https://tkless.github.io/ccm-components/question/question_datastore.js" ],
+      "key": "1"
+    }
+  },
+
+  "2": {
+    "data": {
+      "store": [ "ccm.store", "https://tkless.github.io/ccm-components/question/question_datastore.js" ],
+      "key": "2"
+    }
+  }
+}
