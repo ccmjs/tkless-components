3a0808404a0c1d14f4aa2dc0f3f69e34fdd4a9f7
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-08-29 19:01:53 +0200

    update to ccm v10

--- star_rating_result/ccm.star_rating_result.js
+++ star_rating_result/ccm.star_rating_result.js
@@ -5,15 +5,12 @@
  */
 ( function () {
 
-  var filename = "ccm.star_rating_result.js";
+  var component = {
 
-  var ccm_version = '9.2.0';
-  var ccm_url     = 'https://akless.github.io/ccm/version/ccm-9.2.0.min.js';
+    name: 'star_rating_result',
 
-  var component_name = 'star_rating_result';
-  var component_obj  = {
+    ccm: 'https://akless.github.io/ccm/ccm.js',
 
-    name: component_name,
     config: {
       templates: {
         "main": {
@@ -66,12 +63,11 @@
           ]
         }
       },
-
       data:  {
-          store: [ 'ccm.store', 'https://tkless.github.io/ccm-components/star_rating_result/star_rating_result_datastore.js' ],
+          store: [ 'ccm.store', '../star_rating_result/star_rating_result_datastore.js' ],
           key:   'demo'
       },
-      style: [ 'ccm.load', 'https://tkless.github.io/ccm-components/star_rating_result/style.css' ],
+      style: [ 'ccm.load', '../star_rating_result/style.css' ],
       icons: [ 'ccm.load', { url: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', context: document.head },
         'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' ]
     },
@@ -161,8 +157,5 @@
     }
   };
 
-  if ( window.ccm && window.ccm.files ) window.ccm.files[ filename ] = component_obj;
-  var namespace = window.ccm && ccm.components[ component_name ]; if ( namespace ) { if ( namespace.ccm_version ) ccm_version = namespace.ccm_version; if ( namespace.ccm_url ) ccm_url = namespace.ccm_url; }
-  if ( !window.ccm || !ccm[ ccm_version ] ) { var tag = document.createElement( 'script' ); document.head.appendChild( tag ); tag.onload = register; tag.src = ccm_url; } else register();
-  function register() { ccm[ ccm_version ].component( component_obj ); }
+  function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
 }() );
