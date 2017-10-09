9c33eba6320e2624e0bbe2ea988c9a1d6711a710
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-10-09 21:04:57 +0200

    ccm.editor.js now support code highlighting with highlighting.js

--- editor/ccm.editor.js
+++ editor/ccm.editor.js
@@ -2,6 +2,11 @@
  * @overview ccm component for quill editor
  * @author Tea Kless <tea.kless@web.de>, 2017
  * @license The MIT License (MIT)
+ *
+ *
+ * Quill Editor v1.2.4 https://quilljs.com/
+ * Copyright (c) 2014, Jason Chen
+ * Copyright (c) 2013, salesforce.com
  */
 
 ( function () {
@@ -14,14 +19,19 @@
     ccm: 'https://akless.github.io/ccm/ccm.js',
 
     config: {
-      editor: [ 'ccm.load', '../editor/quill.js' ],
+      // add this line for highlighting the code within editor
+      //code_highlighting: [ 'ccm.load', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/darcula.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'],
+      editor: [ 'ccm.load', '../editor/resources/quill.js' ],
       editor_css: [ 'ccm.load', '//cdn.quilljs.com/1.2.0/quill.snow.css' ],
       settings: {
         modules: {
-          toolbar: [
+          //syntax: true,    // needed for syntax highlighting
+
+          toolbar: [          // if no toolbar needed set: settings.modules.toolbar: false
             [ { header: [ 1, 2, false ] } ],
             [ 'bold', 'italic', 'underline' ],
-            [ 'image', 'code-block' ]
+            [ 'image' ],
+            //['code-block'],  // Include code button in toolbar for syntax highlighting
           ]
         },
         placeholder: 'Write here...',
@@ -33,10 +43,22 @@
       var editor;
 
       this.start = function ( callback ) {
+        if( this.code_highlighting ) hljs.initHighlightingOnLoad();
+
         var div = this.ccm.helper.html( {} );
         this.element.innerHTML = '';
         this.element.appendChild( div );
-        editor = new Quill( div, this.settings );
+
+        editor = new Quill( div, this.settings, function () {
+          if ( this.code_highlighting ) {
+            var customButton = this.element.querySelector( '.ql-code-block' );
+            customButton.addEventListener( 'click', function () {
+              hljs.highlightBlock( this.element.querySelector( 'div[contenteditable=true] > pre' ) );
+            });
+          }
+
+        } );
+
         if ( callback ) callback( this );
       };
       this.get = function () { return editor; }
--- editor/versions/ccm.editor-1.0.0.js
+++ editor/versions/ccm.editor-1.0.0.js
@@ -1,5 +1,6 @@
 /**
  * @overview ccm component for quill editor
+ *
  * @author Tea Kless <tea.kless@web.de>, 2017
  * @license The MIT License (MIT)
  */
@@ -19,14 +20,19 @@
     },
 
     config: {
-      editor: [ 'ccm.load', 'https://tkless.github.io/ccm-components/editor/quill.js' ],
+      // add this line for highlighting the code within editor
+      //code_highlighting: [ 'ccm.load', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/darcula.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'],
+      editor: [ 'ccm.load', 'https://tkless.github.io/ccm-components/editor/resources/quill.js' ],
       editor_css: [ 'ccm.load', '//cdn.quilljs.com/1.2.0/quill.snow.css' ],
       settings: {
         modules: {
-          toolbar: [
+          //syntax: true,    // needed for syntax highlighting
+
+          toolbar: [          // if no toolbar needed set: settings.modules.toolbar: false
             [ { header: [ 1, 2, false ] } ],
             [ 'bold', 'italic', 'underline' ],
-            [ 'image', 'code-block' ]
+            [ 'image' ],
+            //['code-block'],  // Include code button in toolbar for syntax highlighting
           ]
         },
         placeholder: 'Write here...',
@@ -38,10 +44,22 @@
       var editor;
 
       this.start = function ( callback ) {
+        if( this.code_highlighting ) hljs.initHighlightingOnLoad();
+
         var div = this.ccm.helper.html( {} );
         this.element.innerHTML = '';
         this.element.appendChild( div );
-        editor = new Quill( div, this.settings );
+
+        editor = new Quill( div, this.settings, function () {
+          if ( this.code_highlighting ) {
+            var customButton = this.element.querySelector( '.ql-code-block' );
+            customButton.addEventListener( 'click', function () {
+              hljs.highlightBlock( this.element.querySelector( 'div[contenteditable=true] > pre' ) );
+            });
+          }
+
+        } );
+
         if ( callback ) callback( this );
       };
       this.get = function () { return editor; }
