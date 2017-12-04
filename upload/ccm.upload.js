1b952e00abdd9bb7671ab76e7e96bd974f137860
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-12-04 14:17:18 +0100

    file upload component with dropzone.js

--- upload/ccm.upload.js
+++ upload/ccm.upload.js
@@ -1,4 +1,4 @@
-( function () {
+{
 
   var component = {
 
@@ -8,253 +8,121 @@
 
     config: {
       templates: {
-        "main": {
-          "class": "main",
+        "upload": {
           "inner": {
             "class": "container",
-            "id": "container",
-            "inner": [
+            "inner":
               {
-                "id": "actions",
-                "class":"row",
-                "inner": [
+                "class": "row",
+                "inner":[
                   {
-                    "class": "col-lg-7",
-                    "inner": [
-                      {
-                        "tag": "button",
-                        "class": "btn btn-success fileinput-button",
-                        "inner": [
-                          {
-                            "tag": "i",
-                            "class": "glyphicon glyphicon-plus"
-                          },
-                          {
-                            "tag": "span",
-                            "inner": "Add files..."
-                          }
-                        ]
-                      },
-                      {
-                        "tag": "button",
-                        "type": "submit",
-                        "class": "btn btn-primary start",
-                        "inner": [
-                          {
-                            "tag": "i",
-                            "class": "glyphicon glyphicon-upload"
-                          },
-                          {
-                            "tag": "span",
-                            "inner": "Start upload"
-                          }
-                        ]
-                      },
-                      {
-                        "tag": "button",
-                        "type": "reset",
-                        "class": "btn btn-warning cancel",
-                        "inner": [
-                          {
-                            "tag": "i",
-                            "class": "glyphicon glyphicon-ban-circle"
-                          },
-                          {
-                            "tag": "span",
-                            "inner": "Cancel upload"
-                          }
-                        ]
-                      }
-                    ]
+                    "tag": "form",
+                    "id": "upload_widget",
+                    "method": "post",
+                    "action": "%server_url%",
+                    "class": "dropzone"
                   },
                   {
-                    "class": "col-lg-5",
-                    "inner": {
-                      "tag": "span",
-                      "class": "fileupload-process",
-                      "inner": {
-                        "id": "total-progress",
-                        "class": "progress progress-striped active",
-                        "role": "progressbar",
-                        "aria-valuemin": "0",
-                        "aria-valuemax": "100",
-                        "aria-valuenow": "0",
+                    "class": "input-group",
+                    "inner": [
+                      {
+                        "tag": "span",
+                        "class": "input-group-addon",
                         "inner": {
-                          "class": "progress-bar progress-bar-success",
-                          "style": "width:0%",
-                          "data-dz-uploadprogress": true
+                          "tag": "span",
+                          "class": "glyphicon glyphicon-folder-open"
+                        }
+                      },
+                      {
+                        "tag": "input",
+                        "class": "form-control",
+                        "name": "folder",
+                        "type": "text",
+                        "aria-label": "Enter folder name",
+                        "placeholder": "upload folder name ...",
+                        "required": "true"
+                      },
+
+                      {
+                        "tag": "span",
+                        "class": "input-group-btn",
+                        "inner": {
+                          "tag": "button",
+                          "type": "button",
+                          "class": "start btn btn-primary disabled",
+                          "inner": "upload"
                         }
                       }
-                    }
+
+                    ]
                   }
                 ]
-              },
-              {
-                "class": "table table-striped files",
-                "id": "previews",
-                "inner": {
-                  "id": "template",
-                  "class": "file-row",
-                  "inner": [
-                    {
-                      "inner": {
-                        "tag": "span",
-                        "class": "preview img data-dz-thumbnail"
-                      }
-                    },
-                    {
-                      "inner": [
-                        {
-                          "tag": "p",
-                          "class": "name",
-                          "data-dz-name": true
-                        },
-                        {
-                          "tag": "strong",
-                          "class": "error text-danger",
-                          "data-dz-errormessage": true
-                        }
-                      ]
-                    },
-                    {
-                      "inner": [
-                        {
-                          "tag": "p",
-                          "class": "size",
-                          "data-dz-size": true
-                        },
-                        {
-                          "class": "progress progress-striped active",
-                          "role": "progressbar",
-                          "aria-valuemin": "0",
-                          "aria-valuemax":"100",
-                          "aria-valuenow": "0",
-                          "inner": {
-                            "class": "progress-bar progress-bar-success",
-                            "style": "width:0%",
-                            "data-dz-uploadprogress": true
-                          }
-                        }
-                      ]
-                    },
-                    {
-                      "inner": [
-                        {
-                          "tag": "button",
-                          "class": "btn btn-primary start",
-                          "inner": [
-                            {
-                              "tag": "i",
-                              "class": "glyphicon glyphicon-upload"
-                            },
-                            {
-                              "tag": "span",
-                              "inner": "Start"
-                            }
-                          ]
-                        },
-                        {
-                          "tag": "button",
-                          "class": "btn btn-warning cancel",
-                          "data-dz-remove": true,
-                          "inner": [
-                            {
-                              "tag": "i",
-                              "class": "glyphicon glyphicon-ban-circle"
-                            },
-                            {
-                              "tag": "span",
-                              "inner": "Cancel"
-                            }
-                          ]
-                        },
-                        {
-                          "tag": "button",
-                          "class": "btn btn-danger delete",
-                          "data-dz-remove": "true",
-                          "inner": [
-                            {
-                              "tag": "i",
-                              "class": "glyphicon glyphicon-trash"
-                            },
-                            {
-                              "tag": "span",
-                              "inner": "Delete"
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
               }
-            ]
           }
         }
       },
+      uploadBoxMassage: "<span class='glyphicon glyphicon-cloud-upload col-md-12'></span>",
+      url: "http://localhost:8080/upload",
       libs: [ 'ccm.load',
         { context: 'head', url: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' },
         'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
         '../upload/resources/dropzone/dropzone.js',
-        '../upload/resources/dropzone/dropzone.css',
         '../upload/resources/default.css'
       ]
     },
 
     Instance: function () {
-      var self = this;
 
-      this.start = function ( callback ) {
+      this.ready = callback => {
+        $ = this.ccm.helper;
+        callback();
+      };
 
-        self.ccm.helper.setContent( self.element, self.ccm.helper.html( self.templates.main ) );
-        // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-        var previewNode = self.element.querySelector("#template");
-        previewNode.id = "";
-        var previewTemplate = previewNode.parentNode.innerHTML;
-        previewNode.parentNode.removeChild(previewNode);
+      this.start = callback => {
+        $.setContent( this.element, $.html( this.templates.upload, {
+          server_url: this.url
+        } ) );
 
-        var myDropzone = new Dropzone(self.element.querySelector('.main'), { // Make the whole body a dropzone
-          url: "http://localhost:8080/upload", // Set the url
-          thumbnailWidth: 80,
-          thumbnailHeight: 80,
-          parallelUploads: 20,
-          previewTemplate: previewTemplate,
-          autoQueue: false, // Make sure the files aren't queued until manually added
-          previewsContainer: self.element.querySelector( "#previews" ), // Define the container to display the previews
-          clickable: self.element.querySelector('.fileinput-button' ), // Define the element that should be used as click trigger to select files.
+        Dropzone.autoDiscover = false;
+
+        let myDrop = new Dropzone( this.element.querySelector('#upload_widget'), {
+          headers: {
+            "Cache-Control": "",
+            "X-Requested-With": ""
+          },
+          paramName: 'file',
+          maxFileSize: 4, // MB
+          parallelUploads: 100,
+          uploadMultiple: true,
+          autoProcessQueue: false,
+          dictDefaultMessage: this.uploadBoxMassage,
+          dictResponseError: 'Upload faild',
+          acceptedFiles: 'image/*',
         });
 
-        myDropzone.on("addedfile", function(file) {
-          // Hookup the start button
-          file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
+        let folder = this.element.querySelector( 'input[name="folder"]' );
+        myDrop.on( 'sending', ( file, xhr, formData ) => {
+          formData.append( "folder", folder.value );
         });
 
-        // Update the total progress bar
-        myDropzone.on("totaluploadprogress", function(progress) {
-          self.element.querySelector("#total-progress .progress-bar").style.width = progress + "%";
+        myDrop.on( 'success', ( file, resp ) => {
+          console.log( file );
+          console.log( resp );
+          myDrop.removeFile(file);
         });
 
-        myDropzone.on("sending", function(file) {
-          // Show the total progress bar when upload starts
-          self.element.querySelector("#total-progress").style.opacity = "1";
-          // And disable the start button
-          file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+        myDrop.on( 'addedfile', () => {
+          this.element.querySelector( '.start' ).classList.remove( 'disabled' );
         });
 
-        // Hide the total progress bar when nothing's uploading anymore
-        myDropzone.on("queuecomplete", function(progress) {
-          self.element.querySelector("#total-progress").style.opacity = "0";
+        myDrop.on( 'error', () => {
+          this.element.querySelector( '.dz-progress' ).style.background = 'red';
         });
 
-        // Setup the buttons for all transfers
-        // The "add files" button doesn't need to be setup because the config
-        // `clickable` has already been specified.
-        self.element.querySelector("#actions .start").onclick = function() {
-          myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
-        };
-        self.element.querySelector("#actions .cancel").onclick = function() {
-          myDropzone.removeAllFiles(true);
-        };
+        this.element.querySelector( '.start' ).addEventListener( 'click', () => {
+          myDrop.processQueue();
+        });
+
 
         if ( callback ) callback();
       };
@@ -264,4 +132,4 @@
   };
 
   function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
-}() );
+};
