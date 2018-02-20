b2a2b276e06442f1a714b5fa93778476d3232e44
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-02-20 09:33:55 +0100

    change path

--- comment/resources/configs.js
+++ comment/resources/configs.js
@@ -5,7 +5,7 @@ ccm.files[ "configs.js" ] = {
     comment_template: 'expanded',
     data: { store: [ 'ccm.store', { 'store': 'comment', 'url': 'https://ccm.inf.h-brs.de' } ], key: 'demo' },
     user: [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js', { sign_on: 'demo' } ],
-    voting: [ 'ccm.component', '../thumb_rating/ccm.thumb_rating.js', {
+    voting: [ 'ccm.component', 'https://tkless.github.io/ccm-components/thumb_rating/ccm.thumb_rating.js', {
       buttons: true,
       data: { store: [ 'ccm.store', '../voting/resources/datastore.js' ] },
       user: [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/ccm.user.js' ]
@@ -35,5 +35,21 @@ ccm.files[ "configs.js" ] = {
       data: { store: [ 'ccm.store', { 'store': 'voting', 'url': 'ws://localhost:8080' } ] },
       user: [ 'ccm.instance', 'https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js' ]
     } ]
+  },
+
+  "lea": {
+    "comment_template": "expanded",
+    "data": {
+      "store": [ "ccm.store", { "store": "slidecast_comments", "url": "https://ccm.inf.h-brs.de" } ],
+      "key": "learning_apps"
+    },
+    "user": [ "ccm.instance", "https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js", { "sign_on": "demo" } ],
+    "voting": [ "ccm.component", "https://tkless.github.io/ccm-components/thumb_rating/versions/ccm.thumb_rating-1.0.0.min.js",
+      {
+        "buttons": "true",
+        "data": { "store": [ "ccm.store", { "store": "voting", "url": "wss://ccm.inf.h-brs.de" } ] },
+        "user": [ "ccm.instance", "https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js", { "sign_on": "demo" } ]
+      }
+    ]
   }
 };
