0d1a073f1525fd4d56aee60bfb17a81dd30402be
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-03-20 21:36:13 +0100

    minor change

--- pdf-viewer/resources/configs.js
+++ pdf-viewer/resources/configs.js
@@ -1,6 +1,6 @@
 ccm.files[ "configs.js" ] = {
   "demo": {
     pdf: //[ "ccm.get", { url: "https://ccm.inf.h-brs.de", store: "file_upload" }, "1517228670954X509252249813553" ],
-      "//cdn.mozilla.net/pdfjs/tracemonkey.pdf",
+      "//cdn.mozilla.net/pdfjs/tracemonkey.pdf"
   }
 };
