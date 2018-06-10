73b36bb36c44a636afc8211b3520a1bc31e71334
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-06-10 11:42:55 +0200

    crud-app config of pdf-viewer factory

--- pdf_viewer_builder/resources/configs.js
+++ pdf_viewer_builder/resources/configs.js
@@ -40,7 +40,6 @@ ccm.files[ "configs.js" ] = {
     }
   },
 
-
   "crud_app": {
     "builder": [ "ccm.component", "https://ccmjs.github.io/tkless-components/pdf_viewer_builder/ccm.pdf_viewer_builder.js",
       {
@@ -52,7 +51,6 @@ ccm.files[ "configs.js" ] = {
       }
     ],
     "store": [ "ccm.store", { "store": "pdf_viewer", "url": "https://ccm2.inf.h-brs.de", "method": "POST" } ],
-    "url": "https://ccmjs.github.io/tkless-components/pdf_viewer/versions/ccm.pdf_viewer-2.1.0.js",
-    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-3.1.0.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.js", "greedy" ] ]
+    "url": "https://ccmjs.github.io/tkless-components/pdf_viewer/versions/ccm.pdf_viewer-3.0.0.js"
   }
 };
