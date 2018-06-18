6a72e63163076d89aec09ea110f120e1bc88b4f6
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-06-18 14:47:17 +0200

    remove incorrect character

--- exercise_builder/versions/ccm.exercise_builder-2.0.0.js
+++ exercise_builder/versions/ccm.exercise_builder-2.0.0.js
@@ -259,7 +259,7 @@
       },
       "css": [ "ccm.load", "https://ccmjs.github.io/tkless-components/libs/bootstrap/css/bootstrap.css",
         { "context": "head", "url": "https://ccmjs.github.io/tkless-components/libs/bootstrap/css/font-face.css" },
-        "'https://ccmjs.github.io/tkless-components/exercise_builder/resources/default.css"
+        "https://ccmjs.github.io/tkless-components/exercise_builder/resources/default.css"
       ],
       "target": [ "ccm.component", "https://ccmjs.github.io/tkless-components/exercise/versions/ccm.exercise-2.0.0.js", { "submit_button": "Save" } ],
       "submit_button": "Submit",
