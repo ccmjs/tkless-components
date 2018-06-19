eef6e7f328a9a1695dd5681b4359409493637fcc
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-06-19 14:37:58 +0200

    refactoring

--- thumb_rating/ccm.thumb_rating.js
+++ thumb_rating/ccm.thumb_rating.js
@@ -4,7 +4,7 @@
  * @license The MIT License (MIT)
  */
 
-( function () {
+{
 
   var component  = {
 
@@ -13,8 +13,8 @@
     ccm: 'https://ccmjs.github.io/ccm/ccm.js',
 
     config: {
-      data: { store: [ "ccm.store", {} ] },
-      templates: {
+      //"data": { "store": [ "ccm.store", {} ] },
+      "html": {
         "simple": {
           "class": "rating",
           "inner": {
@@ -65,16 +65,17 @@
 
         }
       },
-      libs: [ 'ccm.load',
-        { context: 'head', url: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'},
-        'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
-        { context: 'head', url: '../../ccm-components/libs/bootstrap/css/bootstrap.css' },
-        '../../ccm-components/libs/bootstrap/css/bootstrap.css',
-        '../../../ccm-components/thumb_rating/resources/default.css'
+      "libs": [ "ccm.load",
+        { "context": "head", "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" },
+        "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css",
+        { "context": "head", "url": "../../ccm-components/libs/bootstrap/css/bootstrap.css" },
+        "../../ccm-components/libs/bootstrap/css/bootstrap.css",
+        "../../../ccm-components/thumb_rating/resources/default.css"
       ]
     },
 
     Instance: function () {
+
       /**
        * own reference for inner functions
        * @type {Instance}
@@ -96,6 +97,22 @@
       let total = 0;
 
       this.init = callback => {
+
+        if ( self.user ) self.user.onchange = () => self.start;
+
+        // listen to change event of ccm realtime datastore => (re)render own content
+        self.data.store.onChange =  () => { self.start(); };
+
+        callback();
+      };
+
+      this.ready = callback => {
+
+       /* // listen to login/logout event => (re)render own content
+        if ( self.user ) self.user.addObserver( self.index, function ( event ) {
+          if ( event ) self.start();
+        });*/
+
         // set shortcut to help functions
         $ = self.ccm.helper;
 
@@ -104,29 +121,18 @@
 
         if ( self.logger ) self.logger.log( 'ready', my );
 
-        // listen to change event of ccm realtime datastore => (re)render own content
-        my.data.store.onChange =  () => { self.start(); };
-
-        callback();
-      };
-
-      this.ready = callback => {
-
-        // listen to login/logout event => (re)render own content
-        if ( self.user ) self.user.addObserver( self.index, function ( event ) {
-          if ( event ) self.start();
-        });
-
         callback();
       };
 
       this.start = callback => {
-
         // get dataset for rendering
         $.dataset( my.data, dataset => {
           if( self.logger ) self.logger.log( 'start', dataset );
 
           renderThumbs();
+
+          // perform callback
+          callback && callback();
 
           function renderThumbs() {
 
@@ -134,16 +140,16 @@
             if ( !dataset.likes    ) dataset.likes    = {};
             if ( !dataset.dislikes ) dataset.dislikes = {};
 
-            total = ( Object.keys(dataset.likes).length) - (Object.keys(dataset.dislikes).length);
+            total = ( Object.keys( dataset.likes ).length ) - ( Object.keys( dataset.dislikes ).length );
 
             if ( my.buttons )
-              $.setContent( self.element, $.html( my.templates.buttons, {
+              $.setContent( self.element, $.html( my.html.buttons, {
                 likes: Object.keys( dataset.likes ).length,
                 dislikes: Object.keys( dataset.dislikes ).length
               }));
             else
             // render main html structure
-             $.setContent( self.element, $.html( my.templates.simple, {
+             $.setContent( self.element, $.html( my.html.simple, {
                 likes: Object.keys( dataset.likes ).length,
                 dislikes: Object.keys( dataset.dislikes ).length
               } ) );
@@ -178,15 +184,15 @@
             }
 
             // set click events for like and dislike buttons
-            click( 'likes', 'dislikes' );
-            click( 'dislikes', 'likes' );
+            doVoting( 'likes', 'dislikes' );
+            doVoting( 'dislikes', 'likes' );
 
             /**
              * set click event for like or dislike button
              * @param {string} index - button index ('likes' or 'dislikes')
              * @param {string} other - opposite of index value
              */
-            function click( index, other ) {
+            function doVoting( index, other ) {
 
               // set click event
               div[ index ].addEventListener( 'click', () => {
@@ -195,6 +201,8 @@
                 self.user.login( function () {
 
                   const user = self.user.data().user;
+
+                  if( my.onvote && !my.onvote( { user: user } ) ) return;
 
                   // has already voted?
                   if ( dataset[ index ][ user ] ) {
@@ -226,16 +234,15 @@
             }
           }
 
-          // perform callback
-          if ( callback ) callback();
-
         } );
 
       };
 
       this.getValue = () => total;
+
     }
+
   };
 
-  function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
-}() );
+  function p(){window.ccm[v].component(component)}const f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{const n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{const e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
+}
--- thumb_rating/resources/configs.js
+++ thumb_rating/resources/configs.js
@@ -1,19 +1,17 @@
 ccm.files[ 'configs.js' ] = {
   "demo": {
     "buttons": true,
-    "user":  [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/ccm.user.min.js"],
-    "data":  {
-      "store": [ "ccm.store", "https://ccmjs.github.io/tkless-components/thumb_rating/resources/datastore.js" ],
-      "key": "demo"
-    }
+    "user":  [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-7.0.1.js",
+      [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ]
+    ],
+    "data": { "store": [ "ccm.store", { "store": "thumb_rating" } ], "key": "demo" }
   },
 
   "local": {
     "buttons": true,
-    "user":  [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/ccm.user.min.js"],
-    "data":  {
-      "store": [ "ccm.store", "resources/datastore.js" ],
-      "key": "demo"
-    }
+    "user":  [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-7.0.1.js",
+      [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ]
+    ],
+    "data":  { "store": [ "ccm.store", "resources/datastore.js" ], "key": "demo" }
   }
 };
