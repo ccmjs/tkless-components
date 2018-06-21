92fd4e86501be7a11fa0a35753706ede0eec8ba1
tkless <TGK-fF2-v8c-eCu>
2018-06-21 10:04:03 +0200

    factory for voting component

---
+++ voting_builder/resources/configs.js
@@ -0,0 +1,11 @@
+ccm.files[ "configs.js" ] = {
+  "local": {
+    "submit_button": "Submit",
+    "preview": true,
+    "data": {
+      "store": [ "ccm.store", "../voting_builder/resources/datasets.js" ],
+      "key": "demo"
+    },
+    "onfinish": { "log": true }
+  }
+};
