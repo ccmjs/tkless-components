3e242e887efb9a0037ddcbdd5f1da8e58075e3a2
tkless <TGK-fF2-v8c-eCu>
2018-06-22 12:31:34 +0200

    factory for star rating

---
+++ star_rating_builder/resources/configs.js
@@ -0,0 +1,11 @@
+ccm.files[ "configs.js" ] = {
+  "local": {
+    "submit_button": "Submit",
+    "preview": true,
+    "data": {
+      "store": [ "ccm.store", "../star_rating_builder/resources/datasets.js" ],
+      "key": "demo"
+    },
+    "onfinish": { "log": true }
+  }
+};
---
+++ star_rating_builder/resources/datasets.js
@@ -0,0 +1,8 @@
+ccm.files[ "datasets.js" ] = {
+  "demo": {
+    "user":  [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-7.0.1.js",
+      [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ]
+    ],
+    "data": { "store": [ "ccm.store", { "store": "star_rating" } ], "key": "demo" }
+  }
+};
