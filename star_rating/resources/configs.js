04465f137f64910290b08ee2b1706403610e13e1
tkless <TGK-fF2-v8c-eCu>
2018-06-22 12:04:49 +0200

    refactoring

--- star_rating/resources/configs.js
+++ star_rating/resources/configs.js
@@ -1,11 +1,13 @@
 ccm.files[ "configs.js" ] = {
   "local": {
     data:  {
-      store: [ 'ccm.store', 'https://ccmjs.github.io/tkless-components/star_rating/resources/datastore.js' ],
+      store: [ 'ccm.store', '../star_rating/resources/datastore.js' ],
       key:   'demo'
     },
     star_title: [ "Gefällt mir gar nicht", "Gefällt mir nicht",
        "Ist Ok", "Gefällt mir", "Gefällt mir sehr" ],
-    user:  [ 'ccm.instance', 'https://ccmjs.github.io/akless-components/user/versions/ccm.user-7.0.0.js' ],
+    "user":  [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-7.0.1.js",
+      [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "guest" ]
+    ],
   }
 };
