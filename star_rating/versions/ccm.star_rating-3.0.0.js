f54aaf434594ad3775abef777b2db85fee2bcf16
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-09-14 20:52:59 +0200

    little changes

--- star_rating/versions/ccm.star_rating-3.0.0.js
+++ star_rating/versions/ccm.star_rating-3.0.0.js
@@ -105,7 +105,7 @@
             const input_elem = $.html( my.html.input, {
               id: i,
               star: i,
-              click: async function () { event.preventDefault(); if ( self.user ) await doVoting(); }
+              click: async function ( event ) { event.preventDefault(); if ( self.user ) await doVoting(); }
             } );
 
             if ( self.user && self.user.isLoggedIn() && dataset[ i ] && dataset[ i ][ self.user.data().user ] ) input_elem.checked = true;
@@ -122,7 +122,7 @@
 
         async function doVoting() {
 
-          await self.user.login();
+          await self.user.login( self );
 
           let checked = self.element.querySelector( 'input[name="rating"]:checked' ).value;
 
