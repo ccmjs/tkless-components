9162ab55c2b26b5ff9b848aa4f17ce2e299e4ab2
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-10-31 09:06:32 +0100

    added demo config key

--- slidecast/resources/configs.js
+++ slidecast/resources/configs.js
@@ -1,5 +1,6 @@
 ccm.files[ "configs.js" ] = {
   "demo": {
+    key:"demo",
     slides: [ 'ccm.get',   'https://ccmjs.github.io/tkless-components/slidecast/resources/datasets.js', 'demo_online.slides'],
   },
 
