93dc371b78086f7534ad13d8e86ca9df81a6c652
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-10-31 11:11:18 +0100

    persist exercise demo

--- exercise/resources/configs.js
+++ exercise/resources/configs.js
@@ -10,7 +10,25 @@ ccm.files[ "configs.js" ] = {
     "Handgelenke würde gleich zuschnappen? Er konnte die Aufforderung stehen zu bleiben schon hören.</p>",
     "submit_button": "Save",
     "onfinish": {
-      log: true
+      "log": true,
+      "data": {
+        "store": [ "ccm.store", { "name": "exercise", "url": "https://ccm2.inf.h-brs.de" } ],
+        "key": "demo"
+      },
+    }
+  },
+  "local": {
+    key: "local",
+    task: "<h4>1. Lesen Sie den Text und reflektieren Sie.</h4>" +
+    "<p>Er hörte leise Schritte hinter sich. Das bedeutete nichts Gutes. Wer würde ihm schon folgen, spät in der " +
+    "Nacht und dazu noch in dieser engen Gasse mitten im übel beleumundeten Hafenviertel? Gerade jetzt, wo er das Ding " +
+    "seines Lebens gedreht hatte und mit der Beute verschwinden wollte! Hatte einer seiner zahllosen Kollegen dieselbe " +
+    "Idee gehabt, ihn beobachtet und abgewartet, um ihn nun um die Früchte seiner Arbeit zu erleichtern? Oder gehörten " +
+    "die Schritte hinter ihm zu einem der unzähligen Gesetzeshüter dieser Stadt, und die stählerne Acht um seine " +
+    "Handgelenke würde gleich zuschnappen? Er konnte die Aufforderung stehen zu bleiben schon hören.</p>",
+    "submit_button": "Save",
+    "onfinish": {
+      "log": true
     }
   }
 };
