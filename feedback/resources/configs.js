a25efbdf90673d142df2424ae7563411f24f955e
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-11-09 10:17:09 +0100

    minor change

--- feedback/resources/configs.js
+++ feedback/resources/configs.js
@@ -1,23 +1,23 @@
 ccm.files[ "configs.js" ] = {
 
   "demo": {
-    "from_above": "30",
+    "from_above": 30,
     "data": { "store": [ "ccm.store", { "name": "feedback" } ], "key": "demo" }
   },
 
   "localhost": {
-    "from_above": "30",
+    "from_above": 30,
     "data": { "store": [ "ccm.store", { "name": "feedback", "url": "http://localhost:8080" } ], "key": "demo" }
   },
 
   "local_left": {
-    "from_above": "30",
+    "from_above": 30,
     "css": [ "ccm.load", "https://ccmjs.github.io/tkless-components/feedback/resources/left.css" ],
     "data": { "store": [ "ccm.store", { "name": "feedback" } ], "key": "demo" },
   },
 
   "local_right": {
-    "from_above": "30",
+    "from_above": 30,
     "css": [ "ccm.load", "https://ccmjs.github.io/tkless-components/feedback/resources/right.css" ],
     "data": { "store": [ "ccm.store", { "name": "feedback" } ], "key": "demo" },
   }
