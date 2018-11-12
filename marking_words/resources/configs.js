00266b6c9b83eefd5ff190862da00c2de95106a6
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-11-12 14:22:53 +0100

    minor change

--- marking_words/resources/configs.js
+++ marking_words/resources/configs.js
@@ -1,13 +1,15 @@
 ccm.files[ "configs.js" ] = {
   "demo": {
     "key": "demo",
-    "text": "<h1>Mark words in the text below</h1><p>Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen.</p>" +
+    "text": {
+      "inner": "<h1>Mark words in the text below</h1><p>Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen.</p>" +
       "<p>Manchmal benutzt man Worte wie Hamburgefonts, Rafgenduks oder Handgloves, um Schriften zu testen.</p>" +
       "<p>Manchmal Sätze, die alle Buchstaben des Alphabets enthalten - man nennt diese Sätze »Pangrams«.</p>" +
       "<p>Sehr bekannt ist dieser: The quick brown fox jumps over the lazy old dog.</p>" +
       "<p>Oft werden in Typoblindtexte auch fremdsprachige Satzteile eingebaut (AVAIL® and Wefox™ are testing aussi la Kerning), um die Wirkung in anderen Sprachen zu testen.</p>" +
       "<p>In Lateinisch sieht zum Beispiel fast jede Schrift gut aus. Quod erat demonstrandum. Seit 1975 fehlen in den meisten Testtexten die Zahlen, weswegen nach TypoGb.</p>" +
-      "<p>204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht werden. Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft.</p>",
+      "<p>204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht werden. Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft.</p>"
+    },
     "submit": true,
     "submit_button_label": "Save",
     "retry": true,
