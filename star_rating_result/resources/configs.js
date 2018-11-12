8423612904c516b81f278ece4921fe9ff43c582a
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-11-12 16:18:02 +0100

    added demo config

---
+++ star_rating_result/resources/configs.js
@@ -0,0 +1,11 @@
+ccm.files[ "configs.js" ] = {
+
+  "demo": {
+    "key": "demo",
+    "detailed": true,
+    "data": {
+      "store": [ "ccm.store", "https://ccmjs.github.io/tkless-components/star_rating_result/resources/datastore.js" ],
+      "key": "demo"
+    }
+  }
+};
