1388c8360acaaab0e37230dbf3ce6aaaa5d105c0
tkless <tmeskh2s@smail.inf.h-brs.de>
2019-01-28 18:30:37 +0100

    removed voting from defaults of chat

--- comment_builder/resources/configs.js
+++ comment_builder/resources/configs.js
@@ -2,7 +2,7 @@ ccm.files[ "configs.js" ] = {
 
   "chat": {
     "key": "chat",
-    "html.inner.0.inner": 'Build your Chat Component',
+    "html.inner.0.inner": 'Settings <small>Chat</small>',
     "html.inner.1.inner.4": '',
     "html.inner.1.inner.6": '',
     "html.inner.1.inner.7": '',
@@ -11,7 +11,6 @@ ccm.files[ "configs.js" ] = {
     "preview": true,
     "defaults": {
       "chat": true,
-      "data.store": "[ 'ccm.store',{ 'name':'voting_data', 'url': 'https://ccm2.inf.h-brs.de' } ]",
       "user": "[ 'ccm.instance', 'https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.2.0.js', " +
       "[ 'ccm.get', 'https://ccmjs.github.io/akless-components/user/resources/configs.js', 'compact' ] ]"
     }
