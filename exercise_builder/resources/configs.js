d53af690fae66c9c1eaa0e46550fe98db4141a63
tkless <tmeskh2s@smail.inf.h-brs.de>
2019-01-30 14:59:54 +0100

    changed to user v8.3.1

--- exercise_builder/resources/configs.js
+++ exercise_builder/resources/configs.js
@@ -3,6 +3,10 @@ ccm.files[ "configs.js" ] = {
     "key": "demo",
     "preview": true,
     "submit_button": "Submit",
+    "defaults": {
+      "user": "[ 'ccm.instance', 'https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.3.1.js', " +
+        "[ 'ccm.get', 'https://ccmjs.github.io/akless-components/user/resources/configs.js', 'compact' ] ]"
+    },
     "onfinish": { "log": true }
   }
 };
