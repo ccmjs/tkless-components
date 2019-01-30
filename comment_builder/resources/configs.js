5619cdff2ccbf3c3c01ee13fd9dee6dbb45d7576
tkless <tmeskh2s@smail.inf.h-brs.de>
2019-01-30 10:35:27 +0100

    added config of w&s_chat

--- comment_builder/resources/configs.js
+++ comment_builder/resources/configs.js
@@ -1,8 +1,26 @@
 ccm.files[ "configs.js" ] = {
 
+  "w&s_chat": {
+    "key": "chat",
+    "html.inner.0.inner.inner.1.inner": 'Chat',
+    "html.inner.1.inner.4": '',
+    "html.inner.1.inner.5.inner.1.inner.1": '',
+    "html.inner.1.inner.6": '',
+    "html.inner.1.inner.7": '',
+    "html.inner.1.inner.8": '',
+    "submit_button": false,
+    "preview": false,
+    "defaults": {
+      "chat": true,
+      "data.store": "[ 'ccm.store',{ 'name':'w&s_chat_data', 'url': 'https://ccm2.inf.h-brs.de' } ]",
+      "user": "[ 'ccm.instance', 'https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.2.0.js', " +
+        "[ 'ccm.get', 'https://ccmjs.github.io/akless-components/user/resources/configs.js', 'compact' ] ]"
+    }
+  },
+
   "chat": {
     "key": "chat",
-    "html.inner.0.inner": 'Settings <small>Chat</small>',
+    "html.inner.0.inner.inner.1.inner": 'Chat',
     "html.inner.1.inner.4": '',
     "html.inner.1.inner.6": '',
     "html.inner.1.inner.7": '',
@@ -11,6 +29,7 @@ ccm.files[ "configs.js" ] = {
     "preview": true,
     "defaults": {
       "chat": true,
+      "data.store": "[ 'ccm.store',{ 'name':'chat_data', 'url': 'https://ccm2.inf.h-brs.de' } ]",
       "user": "[ 'ccm.instance', 'https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.2.0.js', " +
       "[ 'ccm.get', 'https://ccmjs.github.io/akless-components/user/resources/configs.js', 'compact' ] ]"
     }
