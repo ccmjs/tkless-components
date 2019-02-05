71fa6e7ca78f9f53fcb849468e02cda86ba4a7d7
tkless <tmeskh2s@smail.inf.h-brs.de>
2019-02-05 15:18:01 +0100

    supports getValue ccm.news.js

--- news/resources/configs.js
+++ news/resources/configs.js
@@ -2,9 +2,10 @@ ccm.files[ "configs.js" ] = {
   "demo" : {
     "key": "demo",
     "data": {
-      "store": [ "ccm.store", { "name": "chat_data", "url": "https://ccm2.inf.h-brs.de" } ],
-      "key": "demo" },
-    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.2.0.js",
+      "store": [ "ccm.store", { "name": "news_data", "url": "https://ccm2.inf.h-brs.de" } ],
+      "key": "demo"
+    },
+    "user": [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.3.1.js",
       [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "compact" ] ],
     "editable": true
   },
--- news/versions/ccm.news-2.1.0.js
+++ news/versions/ccm.news-2.1.0.js
@@ -14,10 +14,23 @@
 
   const component = {
 
+    /**
+     * unique component name
+     * @type {string}
+     */
     name: 'news',
+    version: [ 2,1,0 ],
 
-    ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+    /**
+     * recommended used framework version
+     * @type {string}
+     */
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-20.0.0.js',
 
+    /**
+     * default instance configuration
+     * @type {object}
+     */
     config: {
       templates: {
         "main": {
@@ -95,10 +108,10 @@
         }
 
       },
-      //data:  { store: [ 'ccm.store' ], key: '' },
+      //data:  { store: [  ],  key: '' },
       //user: [ "ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.2.0.js",
       //  [ "ccm.get", "https://ccmjs.github.io/akless-components/user/resources/configs.js", "compact" ] ],
-      css: [ 'ccm.load', '../news/resources/default.css' ],
+      css: [ 'ccm.load', 'https://ccmjs.github.io/tkless-components/news/resources/default.css' ],
       libs: [ 'ccm.load',
         {  context:'head', url: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' },
         'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
