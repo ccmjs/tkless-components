d266217db19f8c204dd214967ef4889c44719920
tkless <tmeskh2s@smail.inf.h-brs.de>
2019-04-24 11:14:06 +0200

    minor change

--- pdf_viewer/resources/configs.js
+++ pdf_viewer/resources/configs.js
@@ -1,6 +1,6 @@
 ccm.files[ "configs.js" ] = {
   "demo": {
     "key": "demo",
-    "pdf": [ "ccm.get", { url: "https://ccm.inf.h-brs.de", name: "ws_pdf_viewer" }, "1556096359223X511708264580848" ]
+    "pdf": [ "ccm.get", { url: "https://ccm2.inf.h-brs.de", name: "ws_pdf_viewer" }, "1556096359223X511708264580848" ]
   }
 };
