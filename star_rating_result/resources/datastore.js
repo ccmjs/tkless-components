325bb4e99504ccfd6a640d61e7b30a0c2f1f3da4
tkless <tmeskh2s@smail.inf.h-brs.de>
2019-04-26 18:03:45 +0200

    minor change

--- star_rating_result/resources/datastore.js
+++ star_rating_result/resources/datastore.js
@@ -1,19 +1,21 @@
 ccm.files[ "datastore.js" ] = {
   "demo": {
     "key": "demo",
-    "5": {
-      "mmuster2s": true,
-      "fsache2s": true,
-      "kschus2m": true
-    },
-    "3": {
-      "mmuster2s": true,
-      "fsache2s": true,
-      "kschus2m": true
-    },
-    "1": {
-      "mmeskh2s": true,
-      "ndemo2m": true
+    "stars": {
+      "5": {
+        "mmuster2s": true,
+        "fsache2s": true,
+        "kschus2m": true
+      },
+      "3": {
+        "mmuster2s": true,
+        "fsache2s": true,
+        "kschus2m": true
+      },
+      "1": {
+        "mmeskh2s": true,
+        "ndemo2m": true
+      }
     }
   }
 };
