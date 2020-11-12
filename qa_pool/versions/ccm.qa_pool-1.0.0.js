34fb1d884960bf5445cf921c11af6358439a9a18
tkless <tmeskh2s@smail.inf.h-brs.de>
2020-11-12 14:03:15 +0100

    correct path to library

--- qa_pool/versions/ccm.qa_pool-1.0.0.js
+++ qa_pool/versions/ccm.qa_pool-1.0.0.js
@@ -1,6 +1,6 @@
 /**
  * @overview ccm component for exam
- * @author Tea Kless <tea.kless@web.de> 2019
+ * @author Tea Kless <tea.kless@web.de> 2020
  * @license The MIT License (MIT)
  */
 
@@ -10,7 +10,6 @@
 
     name: 'qa_pool', version: [ 1, 0, 0 ],
 
-    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-25.0.0.js',
 
     config: {
       "html": [ "ccm.load", "https://ccmjs.github.io/tkless-components/qa_pool/resources/templates.html" ],
@@ -144,7 +143,7 @@
         "realm": "guest",
         "guest": true
       } ],
-      "swap": [ 'ccm.load', "https://SortableJS.github.io/Sortable/Sortable.js" ]
+      "swap": [ "ccm.load", "https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.10.2/Sortable.min.js"]
     },
 
     Instance: function () {
@@ -529,5 +528,5 @@
 
   };
 
-  let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||["latest"])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){window.ccm[c].component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+  let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||[""])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){(c="latest"?window.ccm:window.ccm[c]).component(component);document.head.removeChild(a)};a.src=component.ccm.url}
 } )();
