676cff2168b0e513f50c03bdc45558113357972c
akless <andre.kless@web.de>
2021-09-26 23:05:02 +0200

    minor change

--- pdf_viewer_builder/resources/templates.mjs
+++ pdf_viewer_builder/resources/templates.mjs
@@ -78,26 +78,6 @@ export function main( config, builder, events ) {
           <div id="pvb-labels" class="accordion-collapse collapse" aria-labelledby="pvb-labels-heading" data-bs-parent="#pvb-accordion">
             <div class="accordion-body">
 
-              <!-- Tooltip: First Page -->
-              <div class="mb-3">
-                <div class="heading">
-                  <label for="pvb-first" class="form-label">
-                    ${ builder.text.first }
-                  </label>
-                  <button class="btn btn-inline btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#pvb-first-info" aria-expanded="false" aria-controls="pvb-first-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle-fill text-info" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div class="collapse info" id="pvb-first-info">
-                  <small class="card card-body bg-info p-2">
-                    ${ builder.text.first_info }
-                  </small>
-                </div>
-                <input type="text" name="text.first" class="form-control" id="pvb-first" .value=${ config.text.first }>
-              </div>
-
               <!-- Tooltip: Previous Page -->
               <div class="mb-3">
                 <div class="heading">
