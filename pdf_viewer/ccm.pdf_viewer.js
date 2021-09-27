07c3a8888db6299e64c9910ee9e7a8e86a26130d
akless <andre.kless@web.de>
2021-09-27 15:52:25 +0200

    bugfix

--- pdf_viewer/ccm.pdf_viewer.js
+++ pdf_viewer/ccm.pdf_viewer.js
@@ -215,10 +215,6 @@
          */
         const canvas = this.element.querySelector( 'canvas' );
 
-        // reset canvas size
-        canvas.removeAttribute( 'width' );
-        canvas.removeAttribute( 'height' );
-
         /**
          * current page
          * @type {Object}
