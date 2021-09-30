f3e415910ec2e9228c34241cecb456c232d8fbc3
akless <andre.kless@web.de>
2021-09-30 10:29:55 +0200

    updated ccm.qa_slidecast.js demo

--- qa_slidecast/resources/resources.mjs
+++ qa_slidecast/resources/resources.mjs
@@ -5,32 +5,61 @@
  */
 
 /**
+ * quiz config (used in example slides data)
+ * @type {Object}
+ */
+export const quiz = {
+  "questions": [
+    {
+      "text": "What is HTML?",
+      "input": "radio",
+      "answers": [
+        "Programming Language",
+        { "text": "Hypertext Markup Language", "correct": true },
+        "Something to eat"
+      ]
+    }
+  ],
+  "feedback": true,
+  "progress_bar": false,
+  "placeholder.submit": "Finish"
+};
+
+/**
  * example slides data
  * @type {Object}
  */
 export const slides = [
   {
+    "audio": "./../qa_slidecast/resources/demo/slide1.m4a",
     "content": 1,
-    "audio": "./../qa_slidecast/resources/audio.mp3",
-    "description": "Welcome! The teacher can add a specific description to a slide."
+    "description": "Welcome! The teacher can add an optional description to a slide."
   },
   {
-    "content": "./../qa_slidecast/resources/image.jpg"
+    "audio": "./../qa_slidecast/resources/demo/slide2.m4a",
+    "content": 2,
+    "description": "The description of a slide can also be a separate app (see next slide)."
   },
   {
-    "content": "./../qa_slidecast/resources/logo.jpg"
+    "audio": "./../qa_slidecast/resources/demo/extra.m4a",
+    "content": "./../qa_slidecast/resources/demo/extra.jpg",
+    "description": [ "ccm.proxy", "https://ccmjs.github.io/akless-components/quiz/versions/ccm.quiz-4.1.2.js", quiz ]
   },
   {
-    "content": "./../qa_slidecast/resources/video.mp4",
+    "audio": "./../qa_slidecast/resources/demo/video.m4a",
+    "content": "./../qa_slidecast/resources/demo/video.mp4",
+    "description": "A little video can be placed instead of a slide."
+  },
+  {
+    "audio": "./../qa_slidecast/resources/demo/app.m4a",
+    "content": [ "ccm.proxy", "https://ccmjs.github.io/akless-components/quiz/versions/ccm.quiz-4.1.2.js", quiz ],
+    "description": "A separate app can also be placed instead of a slide."
+  },
+  {
+    "audio": "./../qa_slidecast/resources/demo/slide3.m4a",
+    "content": 3,
     "description": "Commenting is disabled for this slide.",
     "commentary": false
-  },
-  {
-    "content": [ "ccm.proxy", "https://ccmjs.github.io/akless-components/quiz/versions/ccm.quiz-4.1.2.js" ],
-    "description": [ "ccm.proxy", "https://ccmjs.github.io/akless-components/quiz/versions/ccm.quiz-4.1.2.js" ]
-  },
-  {
-    "content": 3
   }
 ];
 
@@ -59,5 +88,8 @@ export const local = {
  * @type {Object}
  */
 export const demo = {
-
+  "ignore": {
+    "slides": slides
+  },
+  "open": "comments"
 };
