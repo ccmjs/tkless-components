ee11729b921699e1ee85ad0cfb6b308de9f41c4d
akless <andre.kless@web.de>
2021-10-02 14:47:32 +0200

    bugfix

--- pdf_viewer/versions/ccm.pdf_viewer-7.0.0.js
+++ pdf_viewer/versions/ccm.pdf_viewer-7.0.0.js
@@ -4,7 +4,7 @@
  * @author Tea Kless <tea.kless@web.de> 2020
  * @author André Kless <andre.kless@web.de> 2021
  * @license The MIT License (MIT)
- * @version latest (7.0.0)
+ * @version 7.0.0
  * @changes
  * version 7.0.0 (30.09.2021): reimplementation by akless
  */
@@ -12,6 +12,7 @@
 ( () => {
   const component = {
     name: 'pdf_viewer',
+    version: [ 7, 0, 0 ],
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-27.1.1.min.js',
     config: {
       "css": [ "ccm.load",
