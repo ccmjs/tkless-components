f51f3dbe47c4e2dbb178940915f6d0c829cb6898
akless <andre.kless@web.de>
2021-10-18 12:25:12 +0200

    bugfix

--- pdf_viewer/resources/templates.mjs
+++ pdf_viewer/resources/templates.mjs
@@ -22,24 +22,24 @@ export function main( instance, events, page_nr, pages ) {
       </div>
       <div>
         <nav id="controls" ?data-hidden=${ pages <= 1 }>
-          <div id="first" title="${ instance.text.first }" @click=${ events.onFirst }>
+          <div id="first" title="${ instance.text.first || '' }" @click=${ events.onFirst }>
             <i class="bi bi-skip-start" ?disabled=${ page_nr === 1 }></i>
           </div>
-          <div id="prev" title="${ instance.text.prev }" @click=${ events.onPrev }>
+          <div id="prev" title="${ instance.text.prev || '' }" @click=${ events.onPrev }>
             <i class="bi bi-caret-left" ?disabled=${ page_nr === 1 }></i>
           </div>
           <div id="jump">
-            <input type="text" size="${ pages.toString().length }" title="${ instance.text.jump }" placeholder="${ page_nr } / ${ pages }" @change=${ events.onJump }>
+            <input type="text" size="${ pages.toString().length }" title="${ instance.text.jump || '' }" placeholder="${ page_nr } / ${ pages }" @change=${ events.onJump }>
           </div>
-          <div id="next" title="${ instance.text.next }" @click=${ events.onNext }>
+          <div id="next" title="${ instance.text.next || '' }" @click=${ events.onNext }>
             <i class="bi bi-caret-right" ?disabled=${ page_nr === pages }></i>
           </div>
-          <div id="last" title="${ instance.text.last }" @click=${ events.onLast }>
+          <div id="last" title="${ instance.text.last || '' }" @click=${ events.onLast }>
             <i class="bi bi-skip-end" ?disabled=${ page_nr === pages }></i>
           </div>
         </nav>
         <nav id="extra">
-          <a href="${ instance.pdf }" download target="_blank" title="${ instance.text.download }" ?data-hidden=${ !instance.downloadable }>
+          <a href="${ instance.pdf }" download target="_blank" title="${ instance.text.download || '' }" ?data-hidden=${ !instance.downloadable }>
             <i class="bi bi-cloud-download"></i>
           </a>
         </nav>
