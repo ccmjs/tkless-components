504755a6cdad2c14a2a0ae9fcc8521b3198415a6
akless <andre.kless@web.de>
2022-02-11 15:01:49 +0100

    ccm.pdf_viewer.js v7.2.0: controllable dark mode

--- pdf_viewer/versions/ccm.pdf_viewer-7.2.0.js
+++ pdf_viewer/versions/ccm.pdf_viewer-7.2.0.js
@@ -4,7 +4,7 @@
  * @author Tea Kless <tea.kless@web.de> 2020
  * @author André Kless <andre.kless@web.de> 2021-2022
  * @license The MIT License (MIT)
- * @version latest (7.2.0)
+ * @version 7.2.0
  * @changes
  * version 7.2.0 (11.02.2022):
  * - controllable dark mode
@@ -23,6 +23,7 @@
 ( () => {
   const component = {
     name: 'pdf_viewer',
+    version: [ 7, 2, 0 ],
     ccm: 'https://ccmjs.github.io/ccm/versions/ccm-27.2.0.min.js',
     config: {
       "css": [ "ccm.load",
