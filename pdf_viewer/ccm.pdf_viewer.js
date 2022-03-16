64d743315052836e23d0919c76ece131eeb4385c
akless <andre.kless@web.de>
2022-03-16 18:17:51 +0100

    bugfix

--- pdf_viewer/ccm.pdf_viewer.js
+++ pdf_viewer/ccm.pdf_viewer.js
@@ -196,7 +196,7 @@
       this.getPages = () => file.numPages;
 
       /** when an observed responsive breakpoint triggers */
-      this.refresh = () => renderPage( true );
+      this.onbreakpoint = this.refresh = () => renderPage();
 
       /** updates main HTML template */
       const render = () => {
@@ -244,7 +244,7 @@
         rendering = false;
 
         // refresh with correct canvas height
-        !no_refresh && $.sleep().then( this.refresh );
+        !no_refresh && $.sleep().then( () => this.refresh( true ) );
 
       };
 
