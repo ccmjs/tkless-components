d86a0d3c308096239d33f54d3f53825edd9e743c
tkless <tmeskh2s@smail.inf.h-brs.de>
2022-05-31 19:10:40 +0200

    update

--- table/versions/ccm.table-5.2.0.js
+++ table/versions/ccm.table-5.2.0.js
@@ -268,7 +268,7 @@
 
                 table_cell.appendChild( $.html( input, {
                   // onchange event for input fields
-                  change: () => {
+                  change: ( event ) => {
                     const target = event.target;
                     self.onchange && self.onchange( { instance: self, target: target } );
                   }
