9a86b78ea0f770b19e576c84d25ff0491788919f
tkless <tmeskh2s@smail.inf.h-brs.de>
2022-11-29 12:14:16 +0100

    update

--- mark_words/ccm.mark_words.js
+++ mark_words/ccm.mark_words.js
@@ -164,7 +164,7 @@
 
         if ( self.data ) {
           const key = self.user ? [ self.data.key, this.user.getValue().key ] : this.data.key;
-          dataset = await $.dataset( key );
+          dataset = await self.data.store.get( key );
         }
 
         if ( !self.inner ) {
