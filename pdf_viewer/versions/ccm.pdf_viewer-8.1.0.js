ab8c676cee26e97e68e1aeefebc7a11c24b6d0be
Andre Kless <andre.kless@web.de>
2022-12-13 15:11:59 +0100

    update

--- pdf_viewer/versions/ccm.pdf_viewer-8.1.0.js
+++ pdf_viewer/versions/ccm.pdf_viewer-8.1.0.js
@@ -392,7 +392,6 @@
                 page: page,
                 linkService: this.linkService
               } );
-              this.element.querySelectorAll( '#annotation-layer img[src="annotation-noicon.svg"]' ).forEach( elem => elem.remove() );
             } );
 
             // Rendering of PDF page is finished.
